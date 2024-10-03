769fa8083f14e7c84d2dc6f52f0609c04e55e0e5
AleksaMorrison <132399902+AleksaMorrison@users.noreply.github.com>
2024-10-03 16:06:20 -0700

    vite

--- qrcode/src/App.vue
+++ qrcode/src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
-  import HomeView from './views/HomeView.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <home-view/>
+  <HomeView />
 </template>
-
-<style scoped>
-</style>
